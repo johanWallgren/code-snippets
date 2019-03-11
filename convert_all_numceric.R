46c353acdca9e41183be3c7b4fd2b6d60a4d80d5
Johan Wållgren <johan.g.wallgren@gmail.com>
2019-03-11 13:12:13 +0100

    Update convert_all_numceric.R

--- convert_all_numceric.R
+++ convert_all_numceric.R
@@ -1,9 +1,9 @@
 # Requiers a data frame named df_raw 
 numeric_columns <-  df_raw %>%
-  na.omit() %>%
-  head(1) %>%
-  mutate_if(is.character, as.numeric) %>%
-  select_if(~ !any(is.na(.))) %>%
-  names()
+  na.omit() %>% # make sure that the top column doesn't have nay NA's
+  head(1) %>% # Select the top column
+  mutate_all(as.numeric) %>% # Converts all to numeric class
+  select_if(~ !any(is.na(.))) %>% # Drops all columns that now contain NA
+  names() # Select only the column names in a list
 
-df <- mutate_at(df_raw, numeric_columns, ~(as.numeric(.))) 
+df <- mutate_at(df_raw, numeric_columns, ~(as.numeric(.))) # Converts all columns in numeric_columns to numeric class
