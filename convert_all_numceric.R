20cafc36cbf2a2f596e002b8027c16c9818e5177
Johan Wållgren <johan.g.wallgren@gmail.com>
2019-03-15 15:26:35 +0100

    made into function

--- convert_all_numceric.R
+++ convert_all_numceric.R
@@ -1,12 +1,16 @@
-# If all data is as char and some should be numeric
-library(tidyverse)
+# If all data is as char but some should be numeric
+# Requiers library(tidyverse)
 
-# Requiers a data frame named df_raw 
-numeric_columns <-  df_raw %>%
-  na.omit() %>% # make sure that the top column doesn't have nay NA's
-  head(1) %>% # Select the top column
-  mutate_all(as.numeric) %>% # Converts all to numeric class
-  select_if(~ !any(is.na(.))) %>% # Drops all columns that now contain NA
-  names() # Select only the column names in a list
-
-df <- mutate_at(df_raw, numeric_columns, ~(as.numeric(.))) # Converts all columns in numeric_columns to numeric class
+char2integer <- function(df){
+  
+  options(warn=-1) # No warnings
+  
+  numeric_columns <-  df %>%
+    na.omit() %>% # make sure that the top column doesn't have nay NA's
+    head(1) %>% # Select the top column
+    mutate_all(as.numeric) %>% # Converts all to numeric class
+    select_if(~ !any(is.na(.))) %>% # Drops all columns that now contain NA
+    names() # Select only the column names in a list
+  
+  mutate_at(df, numeric_columns, ~(as.numeric(.))) # Converts all columns in numeric_columns to numeric class
+}
