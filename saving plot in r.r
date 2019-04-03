b2d86b52fcbdee564fc606275bfdb72ec8a1b547
Johan Wållgren <johan.g.wallgren@gmail.com>
2019-04-03 10:11:45 +0200

    Add files via upload

---
+++ saving plot in r.r
@@ -0,0 +1,8 @@
+# https://ggplot2.tidyverse.org/reference/ggsave.html
+
+# ggplot(aes()) +
+# geom_xxx() +
+ggsave('name_of_plot', 
+         device='png', # "pdf", "jpeg", "tiff", "png", "bmp", "svg", ...
+         path='./folder_for_plots', # Folder must exist before saving plot
+         width=25, height=15, units='cm', dpi=300) # dpi: "retina" (320), "print" (300), or "screen" (72)
