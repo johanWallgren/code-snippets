855fcfcaf8a6ec4c3eee4ca43d276f306fc028f2
Johan Wållgren <johan.g.wallgren@gmail.com>
2019-04-03 10:12:44 +0200

    Update saving plot in r.r

--- saving plot in r.r
+++ saving plot in r.r
@@ -4,5 +4,5 @@
 # geom_xxx() +
 ggsave('name_of_plot', 
          device='png', # "pdf", "jpeg", "tiff", "png", "bmp", "svg", ...
-         path='./folder_for_plots', # Folder must exist before saving plot
+         path='./folder_for_plots', # Adding to current path, folder must exist before saving plot
          width=25, height=15, units='cm', dpi=300) # dpi: "retina" (320), "print" (300), or "screen" (72)
