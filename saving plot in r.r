261f1baa01f1d251d3c963c2420c9f02be8458b2
Johan Wållgren <johan.g.wallgren@gmail.com>
2019-04-03 10:15:46 +0200

    Update saving plot in r.r

--- saving plot in r.r
+++ saving plot in r.r
@@ -3,6 +3,6 @@
 # ggplot(aes()) +
 # geom_xxx() +
 ggsave('name_of_plot', 
-         device='png', # "pdf", "jpeg", "tiff", "png", "bmp", "svg", ...
+         device='png', # 'pdf', 'jpeg', 'tiff', 'png', 'bmp', 'svg', ...
          path='./folder_for_plots', # Adding to current path, folder must exist before saving plot
-         width=25, height=15, units='cm', dpi=300) # dpi: "retina" (320), "print" (300), or "screen" (72)
+         width=25, height=15, units='cm', dpi='print') # dpi 'retina'=320, 'print'=300 or 'screen'=72
